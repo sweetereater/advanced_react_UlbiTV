3b95fd0924f30438985f4689dcdaed59678d35e0
sweetereater <sweeter_2093@mail.ru>
2026-01-15 17:52:32 +0300

    Начало курса, установка и базовая настройка webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  
+  mode: 'production',
+  
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+}

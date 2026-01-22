ccf7b80baf2687915c14faf5d397403864e44333
sweetereater <sweeter_2093@mail.ru>
2026-01-22 15:36:01 +0300

    Добавлены базовые стили (глобальные и для тем)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react"
+import { lazy, Suspense, useState } from "react"
 import { Link, Route, Routes } from "react-router-dom"
 
 import HomePageAsync from "./pages/Home/HomePageAsync"
@@ -6,15 +6,29 @@ import AboutPageAsync from "./pages/About/AboutPageAsync"
 
 import './index.scss'
 
+const enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
 const Counter = lazy(() => import('./components/Counter/Counter'));
 
 export const App = () => {
+
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       
       <Link to="/">Главная</Link>
       <Link to="/about">О нас</Link>
       <Link to="/counter">К счётчику!</Link>
+
+      <button onClick={toggleTheme} >Сменить тему</button>
 
       <Suspense fallback="Загрузка...">
         <Routes>
